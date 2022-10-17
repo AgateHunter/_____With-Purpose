8ca898177a560346d0bbfb147a9b13feca99cf6e
Natalie Reilly <102992414+AgateHunter@users.noreply.github.com>
2022-10-17 11:37:09 -0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _____With_Purpose.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
